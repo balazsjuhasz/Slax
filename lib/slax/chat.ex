c1b2afd9c8e15cebac5353aa6540ec3992984a3e
Balazs Juhasz <juhaszbal@gmail.com>
2024-07-02 11:36:01 +0200

    users can send messages to the chat

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -47,4 +47,14 @@ defmodule Slax.Chat do
     # )
     # |> Repo.all()
   end
+
+  def change_message(message, attrs \\ %{}) do
+    Message.changeset(message, attrs)
+  end
+
+  def create_message(room, attrs, user) do
+    %Message{room: room, user: user}
+    |> Message.changeset(attrs)
+    |> Repo.insert()
+  end
 end
