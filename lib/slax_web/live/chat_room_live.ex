79686c4e47a19870fec7b52774326b183a33891b
Balazs Juhasz <juhaszbal@gmail.com>
2024-08-09 10:41:37 +0200

    add user avatar_path

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -228,6 +228,14 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
+  defp user_avatar_path(user) do
+    if user.avatar_path do
+      ~p"/uploads/#{user.avatar_path}"
+    else
+      ~p"/images/one_ring.jpg"
+    end
+  end
+
   defp format_date(%Date{} = date) do
     today = Date.utc_today()
 
@@ -294,7 +302,7 @@ defmodule SlaxWeb.ChatRoomLive do
         class="h-10 w-10 rounded rounded cursor-pointer"
         phx-click="show-profile"
         phx-value-user-id={@message.user.id}
-        src={~p"/images/one_ring.jpg"}
+        src={user_avatar_path(@message.user)}
       />
       <div class="ml-2">
         <div class="-mt-1">
