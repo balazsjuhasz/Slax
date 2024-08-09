753f5eedba944d93650e30565b687e79e344f815
Balazs Juhasz <juhaszbal@gmail.com>
2024-08-09 10:55:47 +0200

    extract UserComponents

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -7,6 +7,8 @@ defmodule SlaxWeb.ChatRoomLive do
   alias Slax.Chat.{Message, Room}
   alias SlaxWeb.OnlineUsers
 
+  import SlaxWeb.UserComponents
+
   def render(assigns) do
     ~H"""
     <!-- Sidebar -->
@@ -107,7 +109,7 @@ defmodule SlaxWeb.ChatRoomLive do
                 phx-click="show-profile"
                 phx-value-user-id={@current_user.id}
               >
-                <img src={~p"/images/one_ring.jpg"} class="h-8 w-8 rounded" />
+                <.user_avatar user={@current_user} class="h-8 w-8 rounded" />
                 <span class="hover:underline"><%= @current_user.username %></span>
               </.link>
             </div>
@@ -228,14 +230,6 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
-  defp user_avatar_path(user) do
-    if user.avatar_path do
-      ~p"/uploads/#{user.avatar_path}"
-    else
-      ~p"/images/one_ring.jpg"
-    end
-  end
-
   defp format_date(%Date{} = date) do
     today = Date.utc_today()
 
@@ -298,11 +292,11 @@ defmodule SlaxWeb.ChatRoomLive do
       >
         <.icon :if={@current_user.id == @message.user.id} name="hero-trash" class="h-4 w-4" />
       </button>
-      <img
+      <.user_avatar
+        user={@message.user}
         class="h-10 w-10 rounded rounded cursor-pointer"
         phx-click="show-profile"
         phx-value-user-id={@message.user.id}
-        src={user_avatar_path(@message.user)}
       />
       <div class="ml-2">
         <div class="-mt-1">
